052f054cfb0af64127dcaef48d0cc44bd6ab1f5a
Brett Albright <balbright@learningfuze.com>
2026-03-08 06:33:56 +0000

    sql update

--- database/migrations/0001_omniscient_smiling_tiger.sql
+++ database/migrations/0001_omniscient_smiling_tiger.sql
@@ -1,1 +1,9 @@
-ALTER TABLE "todos" ALTER COLUMN "isCompleted" SET DATA TYPE boolean;
+ALTER TABLE "todos" ALTER COLUMN "isCompleted" DROP DEFAULT;--> statement-breakpoint
+ALTER TABLE "todos"
+ALTER COLUMN "isCompleted"
+SET DATA TYPE boolean
+USING CASE
+  WHEN "isCompleted"::text IN ('1', 'true', 't', 'yes', 'y', 'on') THEN true
+  ELSE false
+END;--> statement-breakpoint
+ALTER TABLE "todos" ALTER COLUMN "isCompleted" SET DEFAULT false;
